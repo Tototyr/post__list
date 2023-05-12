cf264c5afd9fa35b3d7d5a311d1c3c59d495886e
Tototyr <mysilia46@gmail.com>
2023-05-12 16:58:27 +0300

    remove console.log in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import { initializeApp } from './entities/app'
 import { selectPostsWithUsers } from './features'
 import { store } from './store'
 
+
 store.dispatch(initializeApp())
 
 export function App() {
     const postWithUsers = useSelector(selectPostsWithUsers)
-
-    console.log(postWithUsers)
-
+    
     return <div className="App" />
 }
