01c436f399ebc62b76a40dd3436a4a9fd766121c
Tototyr <mysilia46@gmail.com>
2023-06-08 12:53:54 +0300

    create post card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { PostsCard } from './components/posts_cards/posts_cards'
 import { UsersLinks } from './components/users_links/users_links'
 import { initializeApp } from './entities/app'
 import { store } from './store'
@@ -11,6 +12,7 @@ export function App() {
     return (
         <div className="App">
             <UsersLinks />
+            <PostsCard />
         </div>
     )
 }
---
+++ src/components/posts_cards/posts_cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import styles from './posts_card.module.scss'
+
+export function PostsCard() {
+    return (
+        <div className={styles.card_container}>
+            <h2 className={styles.card_title}>
+                Sunt aut facere repellat provident occaecati excepturi optio
+                reprehenderit
+            </h2>
+            <p className={styles.card_text}>
+                Quia et suscipit suscipit recusandae consequuntur expedita et
+                cum reprehenderit molestiae ut ut quas totam nostrum rerum est
+                autem sunt rem eveniet architecto
+            </p>
+        </div>
+    )
+}
